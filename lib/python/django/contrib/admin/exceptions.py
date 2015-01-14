0ab0d1b6a1acd9e1d7058e38a2d34f4a344d57bf
Paul McLanahan <pmac@mozilla.com>
2015-01-13 21:53:30 -0500

    Upgrade Django to 1.6.10.
    This is a move from Django 1.5.9 to 1.6.10. I have done this
    on bedrock witout issue. It may still be a bit harsh to just
    do this here, but 1.5.x is unsuported and there are now unpatched
    security issues in the wild. This should allow playdoh users at least
    a slightly easier path forward.
    
    Not that I've also moved Django from being checked into the repo to
    a submodule. We had issues in the past when the official Django github
    repo changed the hashes of the previous mirror. Things are now stable
    on github for Django, so this seemes easier to update for now.

--- lib/python/django/contrib/admin/exceptions.py
+++
@@ -1,6 +0,0 @@
-from django.core.exceptions import SuspiciousOperation
-
-
-class DisallowedModelAdminToField(SuspiciousOperation):
-    """Invalid to_field was passed to admin view via URL query string"""
-    pass
